515c20e2aa415911c3e5bbff551280756baacae3
黄先阳 <huangxianyang@qinjia001.com>
2023-04-28 23:36:13 +0800

    d

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -3,5 +3,6 @@ PROCESSE=`ps -ef | grep monitor_main.py | grep -v grep | awk '{print $2}' | xarg
 echo "stop finish"
 app_path=$(dirname "$PWD")
 cd $app_path
+source activate ml_app
 nohup python monitor_main.py >> nohup.out 2>&1 &
 echo "starting finish"
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 app_path=$(dirname "$PWD")
 cd $app_path
+source activate ml_app
 nohup python monitor_main.py >> nohup.out 2>&1 &
 echo "starting finish"
